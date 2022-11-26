fde0167438a6d2982fa9928f356cb6718f0bb07e
anemoiaa <georgiypetroff@gmail.com>
2022-11-26 17:55:42 +0400

    refactor: minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header(props) {
     return (
-        <header className="flex justify-between items-center mt-[31px] md:mt-[140px] lg:mt-[144px] font-bold leading-[1.48]">
+        <header className="flex justify-between items-center pt-[31px] md:pt-[140px] lg:pt-[144px] font-bold leading-[1.48]">
             <h1 className="text-[26px] text-[#222731] dark:text-[#fff]">devfinder</h1>
             {props.children}
         </header>
