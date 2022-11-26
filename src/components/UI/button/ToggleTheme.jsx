17f28f92c0943cbc30b9b9140b9841466ca52f3f
anemoiaa <georgiypetroff@gmail.com>
2022-11-26 17:21:00 +0400

    refactor

--- src/components/UI/button/ToggleTheme.jsx
+++ src/components/UI/button/ToggleTheme.jsx
@@ -1,11 +1,18 @@
+import { ReactComponent as IconMoon } from "../../../assets/icon-moon.svg";
+import { ReactComponent as IconSun } from "../../../assets/icon-sun.svg";
 
-function ToggleTheme({title, ...props}) {
+
+function ToggleTheme({ title, ...props }) {
+
     return (
         <button {...props} className="text-lightGrey dark:text-[#fff] hover:text-[#222731] dark:hover:text-[#90A4D4]">
             <span className="inline mr-[16px] text-[13px] tracking-[2.5px]">
                 {title}
             </span>
-            {props.children}
+            {title === "DARK"
+                ? <IconMoon className="inline fill-current" />
+                : <IconSun className="inline fill-current" />
+            }
         </button>
     );
 }
