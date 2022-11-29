944df20dd2b1619c76adcfd4d8b3a3e0cb2ce3b2
anemoiaa <georgiypetroff@gmail.com>
2022-11-30 00:44:06 +0400

    fix: link text flowing out

--- src/components/UI/link/LinkWithIcon.jsx
+++ src/components/UI/link/LinkWithIcon.jsx
@@ -1,11 +1,15 @@
 function LinkWithIcon(props) {
+    let value = props.value;
+    if(value && value.length > 21) {
+        value = value.slice(0, 21) + "...";
+    }
     const color = props.value ? "text-[#4B6A9B] dark:text-[#fff]" : "text-lightGrey dark:text-lightGrey";
     const href = props.value ? props.link : "#";
     return (
         <div className={`${color} mt-[16px] md:mt-[19px]`}>
             {props.children}
             <a href={href} className="text-[13px] leading-[1.48] hover:underline">
-                {props.value || "Not Avalibale"}
+                {value || "Not Avalibale"}
             </a>
         </div>
     );
